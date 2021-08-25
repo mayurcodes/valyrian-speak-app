408d12808d3a4fa0a93da66b447edd238ee8bd19
Maddy246 <29030663+Maddy246@users.noreply.github.com>
2021-08-25 12:35:54 +0530

    script updated

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var divOutput = document.querySelector("#output");
+var errorOutput = document.querySelector("#error-output");
 
 var serverURL = "https://api.funtranslations.com/translate/valyrian.json";
 
-function getTranslationUrl(text){
+function getTranslationUrl(text) {
     return serverURL + "?" + "text=" + text;
 }
 
-function errorHandler(error){
-    console.log("Error occured", error);
+function errorHandler(error) {
+    divOutput.innerText = "";
+    errorOutput.innerText = "There are too many requests sent to the server in a given amount of time. Please try again later.";
 }
 
-function clickEventHandler(){
+function clickEventHandler() {
     var inputText = txtInput.value;
-    fetch(getTranslationUrl(inputText))
-    .then(response => response.json())
-    .then(json => divOutput.innerText = json.contents.translated)
-    .catch(errorHandler)
+    if (inputText != "") {
+        fetch(getTranslationUrl(inputText))
+            .then(response => response.json())
+            .then(json => divOutput.innerText = json.contents.translated)
+            .catch(errorHandler)
+
+            errorOutput.innerText = "";
+    } else {
+        divOutput.innerText = "";
+        errorOutput.innerText = "Please type something...";
+    }
+
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
